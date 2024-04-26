a019035fc1fb03ab749622c6336cee7f4b4d64be
tilonon2 <konatetilonon41@gmail.com>
2024-04-26 21:31:13 +0000

    ajout

--- src/main/java/com/example/hopital/HelloApplication.java
+++ src/main/java/com/example/hopital/HelloApplication.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connexion.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1381, 980);
         stage.setTitle("MIAGE Bien-Être");
         stage.setScene(scene);
---
+++ src/main/java/com/example/hopital/dashboardController.java
@@ -0,0 +1,49 @@
+package com.example.hopital;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class dashboardController {
+    @FXML
+    private Button dashboard;
+
+    @FXML
+    private StackPane principale;
+
+    @FXML
+    private Button utilisateurs;
+
+    // Méthode pour charger la vue dashboard
+    @FXML
+    private void loadDashboard() {
+        loadView("dash.fxml");
+    }
+
+    // Méthode pour charger la vue utilisateurs
+    @FXML
+    private void loadUtilisateurs() {
+        loadView("utilisateurs.fxml");
+    }
+
+    // Méthode générique pour charger des vues
+    private void loadView(String fxmlFile) {
+        try {
+            // Crée un FXMLLoader pour le fichier FXML spécifié
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            // Charge le fichier FXML et obtient la racine (root) de la vue chargée
+            Node view = loader.load();
+            // Met à jour le contenu du StackPane principale avec la vue chargée
+            principale.getChildren().setAll(view);
+        } catch (IOException e) {
+            e.printStackTrace(); // Gestion des erreurs en cas de problème de chargement du fichier FXML
+        }
+    }
+
+
+
+}
