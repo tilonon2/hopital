028a0c78be85135fe7f203113f3e839515438079
Mouhammed Sad Radji <saadradji11@gmail.com>
2024-04-28 10:55:08 +0000

    PetitAjoutPartiel
    Je vais tout mettre ce soir, je fais quelque recherche sur les controllers

--- src/main/java/com/example/hopital/dashController.java
+++ src/main/java/com/example/hopital/dashController.java
@@ -1,5 +1,18 @@
 package com.example.hopital;
 
-public class dashController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class dashController implements Initializable {
+    @FXML
+    private HBox cardLayoout;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
