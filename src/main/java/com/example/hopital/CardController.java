5d850f6c6af4a0ba1642c4bb88223d8a9661ed15
Mouhammed Sad Radji <saadradji11@gmail.com>
2024-04-29 19:42:06 +0000

    Card
    Ajout du controller

--- src/main/java/com/example/hopital/CardController.java
+++ src/main/java/com/example/hopital/CardController.java
@@ -1,4 +1,38 @@
 package com.example.hopital;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import model.Book;
+
 public class CardController{
+
+    @FXML
+    private Label authorName;
+
+    @FXML
+    private Label bookName;
+
+    @FXML
+    private HBox box;
+
+    @FXML
+    private ImageView bookImage;
+
+    //generer les couleurs du cadre Aléatoirement
+
+    private String [] colors = {"#000000", "#000000", "#000000", "#000000"};
+
+    public void setData(Book book){
+        Image image = new Image(getClass().getResourceAsStream(book.getImageSrc()));
+        bookImage.setImage(image);
+
+        bookName.setText(book.getName());
+        authorName.setText(book.getAuthor());
+        box.setStyle("-fx-background-color: " + Color.web(colors[(int) (Math.random() * colors.length)]));
+    }
+
 }
