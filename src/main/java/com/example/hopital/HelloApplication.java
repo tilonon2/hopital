db3616dc97ea3d1ba6840b84a7a8f84258324e3e
Mouhammed Sad Radji <saadradji11@gmail.com>
2024-05-15 21:45:29 +0000

    Autre page connexion et maj
    connexion_new.fxml

--- src/main/java/com/example/hopital/HelloApplication.java
+++ src/main/java/com/example/hopital/HelloApplication.java
@@ -2,17 +2,39 @@ package com.example.hopital;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1381, 980);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("connexion_new.fxml"));
+        Scene scene = new Scene(fxmlLoadepr.load(), 1381, 980);
+
+
+// Debut Autre manière d'implémenter
+
+        //Parent root = FXMLLoader.load(getClass().getResource("connexion_new.fxml"));
+        //stage.initStyle(StageStyle.TRANSPARENT);
+        //Scene scene = new Scene(root);
+
+//Pour met le background en transparent
+        scene.setFill(Color.TRANSPARENT);
+
+//Pour cacher les boutons du haut
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        //Fin Autre manière d'implémenter
+
+
+
+
         stage.setTitle("MIAGE Bien-Être");
         stage.setScene(scene);
         stage.show();
